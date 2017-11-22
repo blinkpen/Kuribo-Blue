ef5c963f069ecd0945a72bb0258067257f85ad56
shitassm <kevingragg85@yahoo.com>
2017-11-22 06:07:51 -0600

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Karibo Blue")> 
+<Assembly: AssemblyDescription("NPC editor for SMBX")> 
+<Assembly: AssemblyCompany("WizTech")> 
+<Assembly: AssemblyProduct("Karibo Blue")> 
+<Assembly: AssemblyCopyright("Copyright © WizTech 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("edb05054-eed4-4daa-8b00-56c1c5280f20")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
